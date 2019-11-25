9c5a847429d740bd86f12ca1c0e11d8cc3e41161
Kirsten Holm <55300324+KirstenHolm@users.noreply.github.com>
2019-11-24 23:45:39 -0500

    Add files via upload

---
+++ NP Registration/Capstone.Tests/NationalParkSqlDAOTest.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Transactions;
+
+namespace Capstone.Tests
+{
+    [TestClass]
+    public class NationalParkSqlDAOTest
+    {
+        #region Test Prep
+
+        protected static string _connectionString  = "Server=.\\SQLEXPRESS;Database=npcampgroundtest;Trusted_Connection=True;";
+
+        private TransactionScope transaction;
+
+        /// <summary>
+        /// Begins the transaction for each test.
+        /// </summary>
+        [TestInitialize]
+        public void BeginTestTranaction()
+        {
+            transaction = new TransactionScope();
+        }
+
+        /// <summary>
+        /// Closes out the test transaction and removes the data it uses.
+        /// </summary>
+        [TestCleanup]
+        public void RollbackTestTranaction()
+        {
+            transaction.Dispose();
+        }
+
+        /// <summary>
+        /// Runs SQL commands from a provided string to the test database.
+        /// </summary>
+        /// <param name="sql"></param>
+        protected static void RunSQLCommand(string sql)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        /// <summary>
+        /// Runs SQL file from a provided filestring to the test database.
+        /// </summary>
+        /// <param name="sql"></param>
+        protected static void RunSQLFile(string sql)
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+            }
+        }
+
+        #endregion
+
+        #region Campground Tests
+
+        [TestMethod]
+        public void GetCampgroundsTest()
+        {
+            string sql = "";
+            RunSQLCommand(sql);
+        }
+
+        #endregion
+    }
+}
---
+++ NP Registration/Capstone/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Security.BusinessLogic;
+using Security.DAO;
+using System;
+using System.IO;
+using Capstone.Menus;
+using Capstone.DAO;
+
+namespace Capstone
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the connection string from the appsettings.json file
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            string connectionString = configuration.GetConnectionString("Npcampground");
+
+            Menu menu = new Menu(connectionString);
+            menu.MainMenu();
+        }
+    }
+}
